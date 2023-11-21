67c4f6c6ccc24a2c1789bb3dbd7b0fd27bc2cb21
Ignacio Buioli <ibuioli@gmail.com>
2023-11-21 13:33:32 -0300

    feat: vite server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import { homedir } from 'os';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
+const viteServerConfig = (host: string) => {
+  let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+  let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+  if (!fs.existsSync(keyPath)) {
+      return {}
+  }
+
+  if (!fs.existsSync(certificatePath)) {
+      return {}
+  }
+
+  return {
+      hmr: {host},
+      host,
+      https: { 
+          key: fs.readFileSync(keyPath),
+          cert: fs.readFileSync(certificatePath),
+      },
+  }
+}
+
 export default defineConfig({
+  server: viteServerConfig('oficina.exemax.ar'),
   plugins: [react()]
 })
+
